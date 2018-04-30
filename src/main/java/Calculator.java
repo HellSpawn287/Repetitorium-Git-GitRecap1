4332f784eef3bd63895ca471018b4ac973e83a0c
Robert Prus <robert.prus87@gmail.com>
2018-05-01 01:31:54 +0200

    Raise method added to the Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,13 +2,24 @@ public class Calculator {
     public float plus(float a, float b) {
         return a + b;
     }
+
     public float minus(float a, float b) {
         return a - b;
     }
+
     public float times(float a, float b) {
         return a * b;
     }
+
     public float by(float a, float b) {
         return a / b;
+    }
+
+    public int raise(int a, int b) {
+        int result = a;
+        for (int i = 1; i < b; i++) {
+            result *= a;
+        }
+        return result;
     }
 }
