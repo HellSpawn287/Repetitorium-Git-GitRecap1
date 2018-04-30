b40d67a10a1d440622f2523d048ddd8dcd22a987
Robert Prus <robert.prus87@gmail.com>
2018-05-01 01:47:01 +0200

    Renamed methods: plus, minus, by

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,13 +2,19 @@ public class Calculator {
     public float plus(float a, float b) {
         return a + b;
     }
+
     public float minus(float a, float b) {
         return a - b;
     }
+
     public float times(float a, float b) {
         return a * b;
     }
+
     public float by(float a, float b) {
+        if (b == 0f) {
+            throw new IllegalArgumentException("Division by 0!");
+        }
         return a / b;
     }
 }
